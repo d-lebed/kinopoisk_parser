527a43bd260b3a15f528f2d06a6019bfbbffafe4
m0thman <dmitry@lebed.me>
2014-04-03 12:22:46 +0300

    Add proxy config

--- lib/kinopoisk_parser.rb
+++ lib/kinopoisk_parser.rb
@@ -7,10 +7,19 @@ require 'kinopoisk/person'
 module Kinopoisk
   SEARCH_URL = "http://www.kinopoisk.ru/index.php?kp_query="
   NotFound   = Class.new StandardError
+  @@proxy = nil
+
+  def self.proxy
+    @@proxy
+  end
+
+  def self.proxy=(value)
+    @@proxy = value
+  end
 
   # Headers are needed to mimic proper request so kinopoisk won't block it
   def self.fetch(url)
-    HTTPClient.new.get url, nil, { 'User-Agent'=>'a', 'Accept-Encoding'=>'a' }
+    HTTPClient.new(proxy: @@proxy).get url, nil, { 'User-Agent'=>'a', 'Accept-Encoding'=>'a' }
   end
 
   # Returns a nokogiri document or an error if fetch response status is not 200
